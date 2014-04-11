3a46da17dd5f6004297656e0130289d0f1efa4d8
j-lar <github@jonathanlarsen.com>
2014-04-10 20:04:42 -0400

    Added logic to check whether the input is an integer or a string

--- ifctl-b.pl
+++ ifctl-b.pl
@@ -1,13 +1,34 @@
 #!/usr/bin/perl
-print "Please enter a number\n";
-chomp($num_1 = <STDIN>);
-print "Please enter another number\n";
-chomp($num_2 = <STDIN>);
-if($num_1 < $letter_2)
+print "Please enter a number or letter\n";
+chomp($input_1 = <STDIN>);
+print "Please enter another number or letter\n";
+chomp($input_2 = <STDIN>);
+if($input_1+0 ne $input_1 && $input_2+0 ne $input_2)
  {
-  print "$num_1 comes before $num_2\n";
- } 
+  $input_1 = uc($input_1);
+  $input_2 = uc($input_2);
+  if($input_1 le $input_2)
+   {
+    print "$input_1 comes before $input_2\n";
+   }
+  else
+   {
+    print "$input_2 comes before $input_1\n";
+   }
+ }
+elsif($input_1+0 eq $input_1 && $input_2+0 eq $input_2)
+ {
+  if($input_1 < $input_2)
+   {
+    print "$input_1 is less than $input_2\n";
+   }
+  else
+   {
+    print "$input_2 less than $input_1\n";
+   }
+ }
 else
  {
-  print "$num_2 comes before $num_1\n";
+  print "Sorry, we can't compare two different data types.\n"
  }
+
