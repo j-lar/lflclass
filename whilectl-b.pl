cc08545bf72a5e23e8ff4743084e3e69dbf6ccf3
j-lar <github@jonathanlarsen.com>
2014-04-15 17:27:16 -0400

    added while control exercise b

---
+++ whilectl-b.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+# ask for two numbers and assign to variables
+print "input a number, please.\n";
+chomp($input_1 = <STDIN>);
+print "input another number, please.\n";
+chomp($input_2 = <STDIN>);
+# loop over the previously assigned variables 
+# and and add one to the first until it is greater
+while($input_1 < $input_2)
+ {
+  print "First input is less than second input, adding 1.\n";
+  $input_1 += 1;
+  print "First input is now adjusted to $input_1\n";
+ }
+print "First input is now equal to second input! We are done.\n";
