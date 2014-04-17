bd1c46697408cf848627613bcac0ca37840cdec5
j-lar <github@jonathanlarsen.com>
2014-04-16 20:24:27 -0400

    Added command to clear screen after first player inputs name. Added uppercase to name variables to match no matter the case entered.

--- loopctl.pl
+++ loopctl.pl
@@ -1,20 +1,23 @@
 #!/usr/bin/perl
 
+$clear_screen = `clear`;
+
 print "Please enter a name.\n";
 chomp($name = <STDIN>);
 print "How many guesses should we allow?\n";
 chomp($num_guesses = <STDIN>);
+print $clear_screen;
 for($count = 1; $count <= $num_guesses; $count++)
  {
   print "Guess the name!\n";
   chomp($guess = <STDIN>);
-  if($guess eq $name)
+  if(uc$guess eq uc$name)
    {
     print "You got it!\n";
     last
    }
   print "Wrong!\n";
  }
-print "The answer was $name.\n";
-print "Your last guess was $guess.\n";
+print "The answer was \U$name.\n";
+print "Your last guess was \U$guess.\n";
 print "Game over.\n";
