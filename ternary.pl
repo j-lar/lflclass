f98c1343d0149c6076fe14e712a82e665dd4e263
j-lar <github@jonathanlarsen.com>
2014-04-21 20:19:30 -0400

    added ternary exercise

---
+++ ternary.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+## Exercise A
+$y = $x < 0 ? 5 : 10;
+
+##Exercise B
+$lastname = ($fname eq "Joe") ? "Smoe" : "Smith";
+
+##Exercise C
+$size = ($length < 12) ? "short" : ($length < 24) ? "medium" : "long";
