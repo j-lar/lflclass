1a4030abc0d98d9cb9d0aced04bb0dba5200e00f
j-lar <github@jonathanlarsen.com>
2014-04-21 20:10:46 -0400

    added two logical operator simple exercises

---
+++ logical-operater-a.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+$coin1 = "nickel";
+$coin2 = "dime";
+
+if((($coin1 eq "nickel") || ($coin2 eq "nickel")) && (($coin1 eq "quarter") || ($coin2 eq "quarter")))
+ {
+  print "You have a $coin1 and a $coin2\n";
+ }
+
+ if(($coin2 eq "dime") || ($coin2 eq "quarter"))
+  {
+   print "You have a $coin2!\n"
+  }
---
+++ logical-operater-b.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+$coin1 = 5;
+$coin2 = 10;
+
+if(($coin1 = 5) || ($coin2 = 5) && ($coin1 = 10) || ($coin2 = 10))
+ {
+  print "You have a nickel and a dime!\n";
+ }
+
+if(($coin1 = 5) || ($coin2 = 5))
+ {
+  print "You have a nickel!\n";
+ }
+elsif(($coin1 = 25) || ($coin2 = 25))
+ {
+  print "You have a quarter!\n";
+ }
