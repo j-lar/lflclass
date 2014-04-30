ecd0f1ee1a4826facc44a88a291bd5e4b0808a85
j-lar <github@jonathanlarsen.com>
2014-04-30 14:39:03 -0400

    added array operator exercise A, push and pop example

---
+++ array-op-a.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+print `clear`;
+print "Please enter your last name\n";
+chomp($input = <STDIN>);
+push @name, $input;
+print "Please enter your first name\n";
+chomp($input = <STDIN>);
+push @name, $input;
+print "Please enter your last name\n";
+chomp($input = <STDIN>);
+push @name, $input;
+
+$m_name = pop @name;
+$f_name = pop @name;
+$l_name = pop @name;
+
+print "\nHello $f_name $m_name $l_name!\n";
