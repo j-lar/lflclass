005d1528790146882e0440cd79358d16b6f2a042
j-lar <github@jonathanlarsen.com>
2014-05-08 19:18:32 -0400

    Simplfied script once I realized I had been taught how to add to a hash Also formatted output, changed to using $count for the while loop

--- hash-a.pl
+++ hash-a.pl
@@ -7,27 +7,18 @@
 #subroutine called print_out() ;passing the hash as an argument. Within
 #the subroutine, print out all five months and the associated holidays.
 
-@hash_array = ();
-
-while ($#hash_array <= 3) {
+$count = 0;
+while ($count < 5) {
 	print "Please enter a month of the year.\n";
     chomp($month = <STDIN>);
     print "Ok, thanks! Now enter the name of a holidy that occurs in that month.\n";
     chomp($holiday = <STDIN>);
-#we could just add things to the hash right here, it's more simple that way
-#but we haven't been shown how to do that yet...
-#    $holiday_hash{ $month } = $holiday;
-	unshift (@hash_array, $holiday);
-	unshift (@hash_array, $month);
+    $holiday_hash{ $month } = $holiday;
+	$count++;
 }
 
-for($i = 0; $i <= $#hash_array; $i+=2) {
-#if we use this way, it's the same as what I proposed above... so how do we 
-#do it the way they are expecting?
-	$holiday_hash{ $hash_array[$i] } = $hash_array[$i+1];
-}
-
-print "\n";
+print `clear`;
+print "Here are your months and Holidays:\n\n";
 
 sub print_out {
     while(($key, $value) = each %holiday_hash) {
