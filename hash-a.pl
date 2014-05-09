303a95b64bce7f5de07cbc18de69e7fb63797a04
j-lar <github@jonathanlarsen.com>
2014-05-09 18:12:47 -0400

    moved [H[J and print message into subroutine

--- hash-a.pl
+++ hash-a.pl
@@ -7,31 +7,31 @@
 #subroutine called print_out() ;passing the hash as an argument. Within
 #the subroutine, print out all five months and the associated holidays.
 
-for ( my $count = 0; $count < 5; $count++ ) {
+for ( my $count = 0 ; $count < 5 ; $count++ ) {
     print "Please enter a month of the year.\n";
     chomp( $month = <STDIN> );
-	#need to check to see if the month has been previously used...
-	#would be cool to append to the month name if it's a duplicate, and
-	#then strip off the appended character when printing it back out.
-	#later...
-	if ( exists $holiday_hash{$month} ) {
-		print "Sorry! I can only do one holiday per month. Hashes, eh?!\n";
-		redo;
-	}
+
+    #need to check to see if the month has been previously used...
+    #would be cool to append to the month name if it's a duplicate, and
+    #then strip off the appended character when printing it back out.
+    #later...
+    if ( exists $holiday_hash{$month} ) {
+        print "Sorry! I can only do one holiday per month. Hashes, eh?!\n";
+        redo;
+    }
     else {
-		print "Ok, thanks! Now enter the name of a holiday that occurs in that month.\n";
-		chomp( $holiday = <STDIN> );
-		$holiday_hash{$month} = $holiday;
-	}
+        print "Ok, thanks! Now enter the name of a holiday that occurs in that month.\n";
+        chomp( $holiday = <STDIN> );
+        $holiday_hash{$month} = $holiday;
+    }
 }
 
-print `clear`;
-print "Here are your months and Holidays:\n\n";
-
 sub print_out {
+    print `clear`;
+    print "Here are your months and Holidays:\n\n";
     while ( ( $key, $value ) = each %holiday_hash ) {
         print "$key\: $value\n";
     }
-	print "\n";
+    print "\n";
 }
 print_out(%holiday_hash);
